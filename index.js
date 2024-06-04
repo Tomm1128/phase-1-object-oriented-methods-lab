dc1f8126401bd20e0b872d527f951dcd915f156d
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-06-04 13:48:12 -0400

    Create doCharity and releasePressStatement methods

--- index.js
+++ index.js
@@ -11,3 +11,11 @@ BoardMember.prototype.veto = function(){
 BoardMember.prototype.approve = function(){
   return "You can do that!"
 }
+
+BoardMember.prototype.doCharity = function(){
+  return "I like to help people."
+}
+
+BoardMember.prototype.releasePressStatement = function(){
+  return "You will see great things from Scuber."
+}
